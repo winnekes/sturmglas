88f0ecf455a9d58f9e261df96cc645e9a3ad390d
winnekes <simona.winnekes@googlemail.com>
2021-08-05 17:11:20 +0200

    Add auth0 login: server-side

---
+++ pages/api/protected.ts
@@ -0,0 +1,6 @@
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+
+export default withApiAuthRequired(async function myApiRoute(req, res) {
+  const { user } = getSession(req, res);
+  res.json({ protected: "My Secret", id: user.sub });
+});
