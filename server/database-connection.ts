02a8e0800630241cfdd730c4fba5abe9f2a3bfe9
winnekes <simona.winnekes@googlemail.com>
2021-08-07 19:20:21 +0200

    Dev: refactor directory structure

--- server/database-connection.ts
+++ server/database-connection.ts
@@ -5,8 +5,8 @@ import {
   createConnection,
   getConnectionOptions,
 } from "typeorm";
-import { User } from "../identity-access/user-entity";
-import { Mood } from "../mood/mood-entity";
+import { User } from "./identity-access/user-entity";
+import { Mood } from "./mood/mood-entity";
 
 export const initializeDatabase = async (
   optionOverrides: Record<string, any> = {}
