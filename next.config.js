9f9fa559871eaefe92ff43e00f9cd554df1036a9
winnekes <simona.winnekes@googlemail.com>
2021-08-07 14:37:01 +0200

    PWA: add functionality

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: true,
 
-  webpack: function (config, options) {
+  webpack: function (config) {
     config.experiments = { topLevelAwait: true };
     return config;
   },
-};
+});
