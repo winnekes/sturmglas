e931490747cba4baf4b10dddfb6b903c1611a65f
winnekes <simona.winnekes@googlemail.com>
2021-08-11 21:02:50 +0200

    UX: prepare floating action button

--- app/components/floating-action.tsx
+++ app/components/floating-action.tsx
@@ -1,15 +1,22 @@
 import { AddIcon } from "@chakra-ui/icons";
 import { Action, Fab } from "react-tiny-fab";
+import { colors } from "../styles/theme";
 
 export const FloatingAction = () => {
   return (
     <Fab
       icon={<AddIcon />}
       event={"hover"}
+      text="test"
       alwaysShowTitle={true}
       onClick={() => {}}
+      mainButtonStyles={{ backgroundColor: colors.ui.background02 }}
     >
-      <Action text="Email" onClick={() => {}} />
+      <Action
+        text="How do you feel today?"
+        onClick={() => {}}
+        style={{ backgroundColor: colors.ui.background02 }}
+      />
     </Fab>
   );
 };
