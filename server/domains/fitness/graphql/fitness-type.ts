9125dec3177592a8e289f5a2da7636ef613688ff
winnekes <simona.winnekes@googlemail.com>
2021-08-17 15:03:37 +0200

    Prettier: modify rule

--- server/domains/fitness/graphql/fitness-type.ts
+++ server/domains/fitness/graphql/fitness-type.ts
@@ -2,19 +2,19 @@ import { Field, Int, ObjectType } from "type-graphql";
 
 @ObjectType()
 class DatasetOutputType {
-  @Field((type) => Int)
+  @Field(type => Int)
   yesterday!: number;
 
-  @Field((type) => Int)
+  @Field(type => Int)
   today!: number;
 }
 
 @ObjectType()
 export class FitnessType {
-  @Field((type) => DatasetOutputType)
+  @Field(type => DatasetOutputType)
   steps!: DatasetOutputType;
 
-  @Field((type) => DatasetOutputType)
+  @Field(type => DatasetOutputType)
   heartRate!: DatasetOutputType;
   //
   // @Field((type) => DatasetOutputType)
