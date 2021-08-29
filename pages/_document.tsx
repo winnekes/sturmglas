489c4aaf09aac5d0650a43a89e5382f4b859a9cb
winnekes <simona.winnekes@googlemail.com>
2021-08-29 23:20:26 +0200

    Feature: questions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(arguments)};
+          d=s.createElement(q);d.src='//d1l6p2sc9645hc.cloudfront.net/gosquared.js';q=
+          s.getElementsByTagName(q)[0];q.parentNode.insertBefore(d,q)}(window,document
+          ,'script','_gs');
+
+          _gs('GSN-438323-G');
+          _gs('set', 'anonymizeIP', true);`,
+            }}
+          />
+        </Head>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <body>
           <Main />
