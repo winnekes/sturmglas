650164533c9f7012132c36af63009bbe4266168b
winnekes <simona.winnekes@googlemail.com>
2021-08-30 21:03:19 +0200

    Feature: share as image

--- app/components/mood/update-mood/share-mood-modal.tsx
+++ app/components/mood/update-mood/share-mood-modal.tsx
@@ -2,16 +2,24 @@ import {
   Box,
   Button,
   Flex,
+  Heading,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Tag,
+  Text,
 } from "@chakra-ui/react";
-import { forwardRef, FunctionComponent, useEffect, useRef } from "react";
+import Image from "next/image";
+import React, { forwardRef, FunctionComponent, useEffect, useRef, useCallback } from "react";
+import { colors } from "../../../styles/theme";
 import { MoodsQuery } from "../../../types/graphql";
-import * as ExportImage from "react-component-export-image";
+import { emotions } from "../../../types/mood";
+import { Panel } from "../../generic/panel";
+import { Subheading } from "../../generic/text/subheading";
+import { DateTime as time } from "luxon";
 
 type Props = {
   mood: MoodsQuery["moods"][number];
@@ -20,40 +28,48 @@ type Props = {
 
 export const ShareMoodModal: FunctionComponent<Props> = ({ mood, onClose }) => {
   const componentRef = useRef<HTMLDivElement>(null);
-  const EI = useRef<typeof ExportImage | undefined>();
+  const EI = useRef<typeof any | undefined>();
 
   useEffect(() => {
     if (typeof window !== undefined) {
-      import("react-component-export-image").then(module => (EI.current = module));
+      import("html-to-image").then(module => (EI.current = module));
     }
   }, []);
 
+  const onButtonClick = useCallback(() => {
+    if (componentRef?.current === null) {
+      return;
+    }
+
+    EI?.current
+      .toPng(componentRef?.current, { cacheBust: true })
+      .then(dataUrl => {
+        const link = document.createElement("a");
+        link.download = "sturmglas.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [componentRef]);
+
   return (
-    <Modal onClose={onClose} isOpen isCentered size="lg">
+    <Modal onClose={onClose} isOpen isCentered size="xl">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Share how you're feeling</ModalHeader>
+        <ModalHeader>Preview</ModalHeader>
 
-        <ComponentToPrint ref={componentRef} />
-        <ModalBody as={Flex} direction="column" justifyContent="center"></ModalBody>
+        <ModalBody as={Flex} direction="column" justifyContent="center">
+          <ComponentToPrint mood={mood} ref={componentRef} />
+        </ModalBody>
         <ModalFooter>
-          <Button onClick={onClose} variant="ghost">
+          <Button onClick={onClose} variant="secondary">
             Cancel
           </Button>
 
-          <Button
-            onClick={() =>
-              window &&
-              EI.current?.exportComponentAsPNG(componentRef, {
-                html2CanvasOptions: {
-                  height: 100,
-                  width: 100,
-                },
-              })
-            }
-            variant="primary"
-          >
-            Download as PNG
+          <Button onClick={onButtonClick} variant="primary">
+            Download as image (png)
           </Button>
         </ModalFooter>
       </ModalContent>
@@ -61,6 +77,48 @@ export const ShareMoodModal: FunctionComponent<Props> = ({ mood, onClose }) => {
   );
 };
 
-const ComponentToPrint = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div ref={ref}>Heey!</div>;
-});
+// eslint-disable-next-line react/display-name
+const ComponentToPrint = forwardRef<HTMLDivElement, { mood: MoodsQuery["moods"][number] }>(
+  (props, ref) => {
+    return (
+      <div ref={ref}>
+        <Box
+          bg="red"
+          bgGradient={`radial(${colors.ui.background01}, ${colors.ui.background02})`}
+          color="gray.200"
+          backgroundAttachment="fixed"
+          textAlign="center"
+          borderRadius="25px"
+        >
+          <Image
+            src={emotions[props.mood.emotion].imageUrl}
+            height="300px"
+            width="300px"
+            priority={true}
+            quality={100}
+          />
+          <Panel>
+            <Subheading fontSize="xs" color="gray.500">
+              {time.fromISO(props.mood.date).toLocaleString(time.DATETIME_SHORT)}
+            </Subheading>
+            <Flex mb={4} spacing={4} justifyContent="center" flexWrap="wrap">
+              {props.mood.tags?.map(tag => (
+                <Tag bg="gray.700" color="gray.500" mr={1} mb={1}>
+                  {tag.name}
+                </Tag>
+              ))}
+            </Flex>
+            <Heading as="h3" size="lg" mb={2}>
+              I feel{" "}
+              <span style={{ color: emotions[props.mood.emotion].color }}>
+                {props.mood.emotion.toLowerCase()}
+              </span>{" "}
+              right now
+            </Heading>
+            <Text whiteSpace="pre-wrap">{props.mood.description}</Text>
+          </Panel>
+        </Box>
+      </div>
+    );
+  }
+);
