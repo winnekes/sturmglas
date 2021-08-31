3ecedfd7ebb52fd125e07c176f99d839c94d1d59
winnekes <simona.winnekes@googlemail.com>
2021-08-31 17:27:20 +0200

    Fix: inline fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap"
             rel="stylesheet"
