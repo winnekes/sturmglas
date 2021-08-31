f5eb2a5cc22492a4c0bd2cf042fd9db74753b95b
winnekes <simona.winnekes@googlemail.com>
2021-08-31 17:24:06 +0200

    Fix: inline fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,17 +12,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-          !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(arguments)};
-          d=s.createElement(q);d.src='//d1l6p2sc9645hc.cloudfront.net/gosquared.js';q=
-          s.getElementsByTagName(q)[0];q.parentNode.insertBefore(d,q)}(window,document
-          ,'script','_gs');
-
-          _gs('GSN-438323-G');
-          _gs('set', 'anonymizeIP', true);`,
-            }}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
